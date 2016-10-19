9ce481fd1d7733069097260adac45d020dd379fa
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-10-18 21:09:04 -0500

    starter files

---
+++ javascripts/main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// You'll be building robots to battle each other.
+// When your user interface first loads, provide 2 text inputs to name the two robots that will do battle.
+// You must also provide a select element underneath each text input so that the user can select one of the 6 robot models you defined.
+// Provide a Attack! button that, when clicked, simply applies the damage output of each robot against the other one.
+// Once either robot's health is <0 display a message that the battle is over, and which one won. For example...
+// The Viper Drone defeated the Behemoth ATV with its flamethrower.
---
+++ javascripts/robotClasses.js
@@ -0,0 +1,19 @@
+// Define three robot type functions (e.g. Drone, Bipedal, ATV).
+function Drone() {
+
+}
+
+function Bipedal() {
+
+}
+
+function ATV (){
+
+}
+// A base Robot function.
+// Each type must have a unique property, for example, if it is aerial or ground based.
+// Define at least 2 specific robot model functions for each type.
+// Give each robot model a different range of health. For example, one model can have health range of 50-80, and another one will have a range of 60-120. 
+// To accomplish this, read about the Math.random() function in JavaScript.
+// Give each robot model a different range of damage they do using the same technique.
+// Functional Requirements
